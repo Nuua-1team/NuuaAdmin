de04bd805f06056d15846654738404d86eeb327d
gmkseta <gmkseta@gmail.com>
2018-10-24 05:31:08 +0900

    인스타랑 트립에도 로그인 안하면 수정 못하도록

--- app/admin/insta_metadata.rb
+++ app/admin/insta_metadata.rb
@@ -11,5 +11,20 @@ ActiveAdmin.register InstaMetadata do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+action_item do
+  link_to "로그아웃",destroy_admin_user_session_path, method: :delete if !current_admin_user.nil?
+end
+action_item do
+  link_to "로그인",new_admin_user_session_path,method: :get if current_admin_user.nil?
+end
+
+
+  index do
+    column :insta_data_id
+    column :insta_location
+
+    actions if !current_admin_user.nil?
+
+  end
 
 end
--- app/admin/trip_metadata.rb
+++ app/admin/trip_metadata.rb
@@ -11,5 +11,20 @@ ActiveAdmin.register TripMetadata do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+action_item do
+  link_to "로그아웃",destroy_admin_user_session_path, method: :delete if !current_admin_user.nil?
+end
+action_item do
+  link_to "로그인",new_admin_user_session_path,method: :get if current_admin_user.nil?
+end
+
+index do
+  column :trip_idx
+  column :trip_review_url
+  column :trip_gallery_id
+
+  actions if !current_admin_user.nil?
+
+end
 menu priority: 3
 end
