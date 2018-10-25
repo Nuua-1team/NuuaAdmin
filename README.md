e96267ffed0677f10594544971d23b89936a261d
gmkseta <gmkseta@gmail.com>
2018-10-25 23:44:15 +0900

    readme에 ㄹ링크추가

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# 누아 이미지 필터링용 관리자 페이지 ... ..
+# 누아 이미지 전처리 관리자 페이지
 
 * ruby 2.4.2
 * rails 5.2.1
+
+
+
+##### <a href="gmkseta.iptime.org:3000">페이지 링크 : gmkseta.iptime.org:3000</a>
--- app/admin/image_infos.rb
+++ app/admin/image_infos.rb
@@ -12,10 +12,10 @@ permit_params :list, :of, :attributes, :on, :model
 #   permitted
 # end
 
-action_item :super_action do
+action_item do
   link_to "로그아웃",destroy_admin_user_session_path, method: :delete if !current_admin_user.nil?
 end
-action_item :super_action do
+action_item do
   link_to "로그인",new_admin_user_session_path,method: :get if current_admin_user.nil?
 end
 menu priority: 2
