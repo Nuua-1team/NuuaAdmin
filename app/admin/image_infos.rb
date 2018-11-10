77db37888efaff2884a7c3823f21b12909e174ac
gmkseta <gmkseta@gmail.com>
2018-11-10 12:39:09 +0900

    아 ㅇ꺼놧따

--- app/admin/image_infos.rb
+++ app/admin/image_infos.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register ImageInfo do
 
-config.current_filters = false
+# config.current_filters = false
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
